bd283bc0f93ffd2e77307b5cd9d215906b925813
Gryshyn Vitaliy <vitaliy.gryshyn@extern.a1.at>
2021-03-13 21:31:56 +0200

    add 3 DB variant

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,20 +1,64 @@
 import React, {Component} from "react";
-import {Jumbotron} from "react-bootstrap";
+import {Button, Card, Jumbotron} from "react-bootstrap";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faList} from "@fortawesome/free-solid-svg-icons";
 
 export default class Welcome extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: "KIDS (2012-2013)",
+        };
+        this.changeDB = this.changeDB.bind(this);
+        this.changeDB = this.changeDB.bind(this);
+    }
+
     componentDidMount() {
-       localStorage.setItem("host", "https://kids-championchip-2012-service.herokuapp.com/ui/")
-     // localStorage.setItem("host", "http://localhost:8092/ui/")
+        localStorage.setItem("host", "https://kids-championchip-2012-service.herokuapp.com/ui/")
+        localStorage.setItem("derff-subTitle", "KIDS (2012-2013)")
+        // localStorage.setItem("host", "http://localhost:8092/ui/")
+    }
+
+    changeDB(dbName, title) {
+       this.setState({
+           title: title,
+       });
+        localStorage.setItem("host", dbName)
+        localStorage.setItem("derff-subTitle", title)
     }
 
     render() {
+        const {title} = this.state;
         return (
             <Jumbotron className="bg-dark text-white">
                 <h1>KIDS-2021 Відкритий Чемпіонат з футболу серед дітей</h1>
+                <h2 style={{"color":"red"}}>{title}</h2>
                 <p>
                     Ресурс для адміністрування змагань сезону 2021
                 </p>
+                <Card className={"text-white"} style={{backgroundColor: 'transparent'}}>
+                    <Card.Header>
+
+                        {'  '}<Button size="sm" variant="info" type="button"
+                                      style={{"display": "inline"}}
+                                      onClick={this.changeDB.bind(this, "https://kids-championchip-2008-service.herokuapp.com/ui/", "ADULT (2008-2009)")}>
+                        <FontAwesomeIcon icon={faList}/> 2008 рік
+                    </Button>
+                        {'  '}<Button size="sm" variant="info" type="button"
+                                      style={{"display": "inline"}}
+                                      onClick={this.changeDB.bind(this, "https://kids-championchip-2010-service.herokuapp.com/ui/", "JUNIOR (2010-2011)")}>
+                        <FontAwesomeIcon icon={faList}/> 2010 рік
+                    </Button>
+                        {'  '}<Button size="sm" variant="info" type="button"
+                                      style={{"display": "inline"}}
+                                      onClick={this.changeDB.bind(this, "https://kids-championchip-2012-service.herokuapp.com/ui/", "KIDS (2012-2013)")}>
+                        <FontAwesomeIcon icon={faList}/> 2012 рік
+                    </Button>
+                    </Card.Header>
+                    <Card.Body>
+                    </Card.Body>
+                </Card>
             </Jumbotron>
         );
     }
